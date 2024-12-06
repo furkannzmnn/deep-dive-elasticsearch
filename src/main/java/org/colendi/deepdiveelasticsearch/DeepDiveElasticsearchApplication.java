c1668c4b0b083efeb206b14527acaee3dbe59128
furkannzmnn <ozmenf97@gmail.com>
2024-12-06 21:15:31 +0300

    Initializes Spring Boot project with Elasticsearch mappings and analyzers.

--- src/main/java/org/colendi/deepdiveelasticsearch/DeepDiveElasticsearchApplication.java
+++ src/main/java/org/colendi/deepdiveelasticsearch/DeepDiveElasticsearchApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeepDiveElasticsearchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeepDiveElasticsearchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeepDiveElasticsearchApplication.class, args);
+    }
 
 }
--- src/test/java/org/colendi/deepdiveelasticsearch/DeepDiveElasticsearchApplicationTests.java
+++ src/test/java/org/colendi/deepdiveelasticsearch/DeepDiveElasticsearchApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DeepDiveElasticsearchApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
