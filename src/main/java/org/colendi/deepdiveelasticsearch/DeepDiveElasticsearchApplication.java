f86fd3080ad3a4fd3f6952ffd302a42684eba724
furkannzmnn <ozmenf97@gmail.com>
2024-12-06 21:15:26 +0300

    Initial commit

---
+++ src/main/java/org/colendi/deepdiveelasticsearch/DeepDiveElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package org.colendi.deepdiveelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepDiveElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepDiveElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/colendi/deepdiveelasticsearch/DeepDiveElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.colendi.deepdiveelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepDiveElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
